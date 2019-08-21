261cc904aa222253408023606d968a8130e249aa
amro eldewiny <amroeldewiny@gmail.com>
2019-08-22 00:57:11 +0200

    add register with firebase

--- src/app/auth/sign-up/sign-up.page.ts
+++ src/app/auth/sign-up/sign-up.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from 'firebase'; 
 
 @Component({
   selector: 'app-sign-up',
@@ -8,18 +11,34 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignUpPage implements OnInit {
 
+  user: User;
+
   signup = new FormGroup({
     'email': new FormControl('', Validators.required),
     'password': new FormControl('', Validators.required)
   });
 
-  constructor() { }
+  constructor(private router: Router, public afAuth: AngularFireAuth ) { }
 
   ngOnInit() {
   }
 
-  signupForm() {
+  async signupForm() {
+    try {
+      console.log(this.signup);
+      let email = this.signup.value.email;
+      let password = this.signup.value.password;
+
+      await this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      this.router.navigateByUrl('home');
+    } catch (error) {
+      console.log(error);
+    }
     console.log(this.signup);
+    let email = this.signup.value.email;
+    let password = this.signup.value.password;
+    this.router.navigateByUrl('home');
+    this.afAuth.auth.signInWithEmailAndPassword(email, password)
   }
 
 }
